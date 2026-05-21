767db5f1fe3c273195a2c6412c23d19a88819478
karamuser <albarekaram206@gmail.com>
2026-05-22 00:59:42 +0300

    Update simple_os.py
    NEW UPDATE FOR SIMPLE OS!

--- simple_os.py
+++ simple_os.py
@@ -1,7 +1,8 @@
+
 import time
 #الاساس 
-print(" hello and welcome to SIMPLE OS with py by karam ... v.0.1...")
-print("    powered by OS SYS ")
+print(" hello and welcome to SIMPLE OS with py by karam ... v.0.2...")
+print("                  {powered by OS SYS} ")
 time.sleep(2)
 print("start OS")
 time.sleep(5)
@@ -12,9 +13,9 @@ def neofetch():
     print("""
 \033[96m
 ooOOOOoo       ssSSSSss
-oo.   oo       ss
-oo.   oo         ssssss
-ooOOOOoo       ssssssss
+oo    oo       ss
+oo    oo         ssssss:
+ooOOOOoo       ssssssss:
 \033[0m
 ╔════════════════════════════════╗
 ║ \033[93mOS:\033[0m        S.OS v0.1                 ║
@@ -24,6 +25,30 @@ ooOOOOoo       ssssssss
 ║ \033[93mColors:\033[0m       Hacker Green           ║
 ╚════════════════════════════════╝
 """)
+# calc in OS 👀
+def calc():
+    # calc brain 
+    num1 = float(input(" enter the number>:  "))
+    oP = input(" enter the operation>: ")
+    num2 = float(input(" enter the number2>: "))
+    #calc oP
+    if oP == "+":
+        print(num1+num2)
+    elif oP == "-":
+        print(num1-num2)
+    elif oP == "×":
+        print(num1*num2) 
+    # stupid error      
+    elif oP == "÷":
+        if num2 and num1 != "0":
+            print(num1/num2)
+        else:
+            print("error! stupid question bro....SON 🥀")
+    else:
+        print("error.in.input!")
+#end
+
+# ترا عندي حياة 😭 ✌️ 
 
 #info 
 text = '''
@@ -34,12 +59,17 @@ this OS like Dos and early linux the kernel is simple and easy \n you don't need
 while True:
     user_input = input("S_os$~/ ")
     if user_input == "help":
-        print("info \n exit \n neofetch_larp\n you can use IT in this OS ") 
+        print("info \n exit \n neofetch_larp\n calc\n snake_game\n you can use IT in this OS ") 
     elif user_input == "info":
         print(text)  
     elif user_input == "neofetch_larp":
-        neofetch()  
+        neofetch()
+    elif user_input == "calc":
+        calc()
+    elif user_input == "snake_game":
+        snake_game()    
     elif user_input == "exit":
         break  
     else:
         print("ERROR$404")  
+    
